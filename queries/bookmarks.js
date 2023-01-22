c0e9ae7ab3d5ba5fa7139e7bdaa629b6e9278ae2
NikeshW <nikeshwankhade@pursuit.org>
2023-01-22 14:56:17 -0500

    added CRUD functionality

--- queries/bookmarks.js
+++ queries/bookmarks.js
@@ -31,4 +31,36 @@ const createBookmark = async (bookmark) => {
   }
 };
 
-module.exports = { getAllBookmarks, getBookmark, createBookmark };
+// DELETE
+const deleteBookmark = async (id) => {
+  try {
+    const deletedBookmark = await db.one(
+      "DELETE FROM bookmarks WHERE id=$1 RETURNING *",
+      id
+    );
+    return deletedBookmark;
+  } catch (error) {
+    return error;
+  }
+};
+
+// UPDATE
+const updateBookmark = async (id, bookmark) => {
+  try {
+    const updatedBookmark = await db.one(
+      "UPDATE bookmarks SET name=$1, url=$2, category=$3, is_favorite=$4 WHERE id=$5 RETURNING *",
+      [bookmark.name, bookmark.url, bookmark.category, bookmark.is_favorite, id]
+    )
+    return updatedBookmark;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  getAllBookmarks,
+  getBookmark,
+  createBookmark,
+  deleteBookmark,
+  updateBookmark
+};
